024c049e608e70fbaf2bea9599dc8d02218b4114
Joao-Normando <joao.normando@hotmail.com>
2021-08-30 15:45:09 -0300

    modificando classe formatando e finalizando pratica

--- Classes/SerieRepositorio.cs
+++ Classes/SerieRepositorio.cs
@@ -8,34 +8,35 @@ namespace DIO.Series
     {
 
         private List<Serie> listaSerie = new List<Serie>();
-        public void Atualiza(int id, Serie entidade)
+        public void Atualiza(int id, Serie objeto)
         {
-            throw new System.NotImplementedException();
+            listaSerie[id] = objeto;
         }
 
         public void Exclui(int id)
         {
-            throw new System.NotImplementedException();
+            listaSerie[id].Excluir();
+            
         }
 
-        public void Insere(Serie entidade)
+        public void Insere(Serie objeto)
         {
-            throw new System.NotImplementedException();
+            listaSerie.Add(objeto);
         }
 
         public List<Serie> Lista()
         {
-            throw new System.NotImplementedException();
+           return listaSerie;
         }
 
         public int ProximoId()
         {
-            throw new System.NotImplementedException();
+            return listaSerie.Count;
         }
 
         public Serie RetornaPorId(int id)
         {
-            throw new System.NotImplementedException();
+            return listaSerie[id];
         }
     }
 
